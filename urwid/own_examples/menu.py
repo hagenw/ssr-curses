b41e83c2169f9ea4a1460a331020a2cd22331bb8
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-10-03 18:57:19 +0200

    Add menu entries via function

--- urwid/own_examples/menu.py
+++ urwid/own_examples/menu.py
@@ -1,5 +1,11 @@
 import urwid
 
+entries = [
+        ('1', 'Play'),
+        ('2', 'Stop'),
+        ('3', 'Open'),
+        ('4', 'Quit'),]
+    
 def exit_on_q(key):
     if key in ('q', 'Q'):
         raise urwid.ExitMainLoop()
@@ -15,26 +21,16 @@ loop = urwid.MainLoop(placeholder, palette, unhandled_input=exit_on_q)
 loop.widget = urwid.AttrMap(placeholder, 'bg')
 loop.widget.original_widget = urwid.Filler(urwid.Columns([]),valign='bottom')
 
-# menu entries
-txt_play  = urwid.Text(('entry_text', u"Play "), align='left')
-txt_pause = urwid.Text(('entry_text', u"Pause "), align='left')
-txt_open  = urwid.Text(('entry_text', u"Open "), align='left')
-txt_quit  = urwid.Text(('entry_text', u"Quit "), align='left')
-txt_f1 = urwid.Text(('entry_button', u" F1"), align='left')
-txt_f2 = urwid.Text(('entry_button', u" F2"), align='left')
-txt_f3 = urwid.Text(('entry_button', u" F3"), align='left')
-txt_f4 = urwid.Text(('entry_button', u" F4"), align='left')
-entry1 = urwid.Columns([txt_f1, txt_play])
-entry1 = urwid.AttrMap(entry1, 'streak')
-entry2 = urwid.Columns([txt_f2, txt_pause])
-entry2 = urwid.AttrMap(entry2, 'streak')
-entry3 = urwid.Columns([txt_f3, txt_open])
-entry3 = urwid.AttrMap(entry3, 'streak')
-entry4 = urwid.Columns([txt_f4, txt_quit])
-entry4 = urwid.AttrMap(entry4, 'streak')
-
+def add_menu_entry(number, name):
+    button_txt = urwid.Text(('entry_text', name), align='left')
+    button_key = urwid.Text(('entry_button', u" F" + number), align='left')
+    # 'pack' calculates the needed size of the column with F1 etc.
+    entry = urwid.Columns([('pack', button_key), button_txt])
+    return urwid.AttrMap(entry, 'streak')
+    
 columns = loop.widget.base_widget # .base_widget skips the decorations
-for item in [entry1, entry2, entry3, entry4]:
+for item in [add_menu_entry('1','Play'), add_menu_entry('2','Pause'),
+        add_menu_entry('3','Open'), add_menu_entry('4','Quit')]:
     columns.contents.append((item, columns.options()))
 
 loop.run()
